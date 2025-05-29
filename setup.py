a14246a0fc635cdb114cd28ba651f25d748a2eb8
Tomasz Kowalczewski <tkowalczewski@voltdb.com>
2025-05-29 17:29:49 +0200

    Trying to fix mkdocs not seeing the package.

--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     version="1.0",
     packages=find_packages(),
     install_requires=["Markdown>=3.0"],
+    entry_points={
+        "markdown.extensions": [
+            "replace_vars = replace_vars:makeExtension"
+        ]
+    }
 )
